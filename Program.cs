cc545f3e8d45335765db40d7418ef270493446a5
Dan Arnaiz <dfarnaiz@gmail.com>
2024-06-25 10:15:49 +0800

    SignInForms

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace interventory
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SignInForm());
         }
     }
 }
